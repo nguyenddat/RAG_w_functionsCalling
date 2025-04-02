ea6db0b791c0afcf93b40cfd5676a748e70c6c5c
Đạt Đình <datnd.b23cc032@stu.ptit.edu.vn>
2025-04-02 18:25:09 +0700

    add more functions

---
+++ functions/hoc_bong.py
@@ -0,0 +1,38 @@
+import os
+
+from rag import RagMini
+from retriever import Retriever
+from core.models import get_chat_completion
+
+data_path = os.path.join(
+    os.getcwd(), "rag_n_function_calling", "data", "txt_data", "Học bổng"
+)
+
+class HocBong(RagMini):
+    def __init__(self):
+        self.retriever = Retriever(data_path)
+        self.description = "Cung cấp thông tin về các loại học bổng của trường mà bạn cần biết."
+
+    def get_k_relevant(self, query: str, k: int) -> str:
+        docs = self.retriever.retriever.invoke(
+            query, config={"k": k}
+        )
+
+        return "\n".join([doc.page_content for doc in docs])
+    
+
+    def invoke(self, question: str) -> str:
+        context = self.get_k_relevant(question, 4)
+        response = get_chat_completion(
+            message = self.message, 
+            task = "rag", 
+            params = {"question": question, "context": context}
+        )
+
+        return response
+
+hoc_bong_rag = HocBong()
+
+        
+
+
--- test.py
+++ test.py
@@ -4,7 +4,10 @@ from functions import (
     phuong_thuc_xet_tuyen,
     quy_doi_chung_chi,
     diem_trung_tuyen,
-    thong_tin_truong
+    thong_tin_truong,
+    hoc_bong,
+    ky_tuc_xa,
+    le_phi
 )
 from core.models import get_chat_completion
 
@@ -14,7 +17,10 @@ functions = {
     "QuyDoiChungChi": quy_doi_chung_chi.quy_doi_chung_chi_rag,
     "NganhVaChuyenNganh": nganh_hoc.nganh_va_chuyen_nganh_rag,
     "DiemTrungTuyen": diem_trung_tuyen.diem_trung_tuyen_rag,
-    "ThongTinTruong": thong_tin_truong.thong_tin_truong_rag
+    "ThongTinTruong": thong_tin_truong.thong_tin_truong_rag,
+    "HocBong": hoc_bong.hoc_bong_rag,
+    "KyTucXa": ky_tuc_xa.ky_tuc_xa_rag,
+    "LePhi": le_phi.le_phi_rag
 }
 
 functions_description = "\n".join(
